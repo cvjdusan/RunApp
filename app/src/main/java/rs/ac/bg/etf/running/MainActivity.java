0d44f03220bc790b03c9fb3090089c19c686f4b1
cvjdusan <61889682+cvjdusan@users.noreply.github.com>
2021-04-27 02:40:15 +0200

    Login and start of nav. drawer

--- app/src/main/java/rs/ac/bg/etf/running/MainActivity.java
+++ app/src/main/java/rs/ac/bg/etf/running/MainActivity.java
@@ -1,20 +1,29 @@
 package rs.ac.bg.etf.running;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import rs.ac.bg.etf.running.databinding.ActivityMainBinding;
 import rs.ac.bg.etf.running.workouts.WorkoutListFragmentDirections;
 
 @AndroidEntryPoint
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     public static final String LOG_TAG = "running-app-example";
 
@@ -22,14 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        if (savedInstanceState == null) {
-            setupNavigation();
-        }
+
+        this.configureToolBar();
+
+        this.configureDrawerLayout();
+
+        this.configureNavigationView();
+
+
+
+
+
+//        if (savedInstanceState == null) {
+//            setupNavigation();
+//        }
 //
 //        if (getIntent().getAction().equals(INTENT_ACTION_WORKOUT)) {
 //            NavController navController = BottomNavigationUtil
@@ -40,24 +64,60 @@ public class MainActivity extends AppCompatActivity {
 //        }
     }
 
+    private void configureToolBar(){
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_app_bar_open_drawer_description, R.string.nav_app_bar_open_drawer_description);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        setupNavigation();
+        //setupNavigation();
     }
 
-    private void setupNavigation() {
-        int[] navResourceIds = new int[]{
-                R.navigation.navigation_routes,
-                R.navigation.navigation_workouts,
-                R.navigation.navigation_calories
-        };
-        BottomNavigationUtil.setup(
-                binding.bottomNavigation,
-                getSupportFragmentManager(),
-                navResourceIds,
-                R.id.nav_host_container
-        );
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.navigation.navigation_routes :
+                break;
+            case R.navigation.navigation_calories:
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
+
+//    private void setupNavigation() {
+//        int[] navResourceIds = new int[]{
+//                R.navigation.navigation_routes,
+//                R.navigation.navigation_workouts,
+//                R.navigation.navigation_calories
+//        };
+//        BottomNavigationUtil.setup(
+//                binding.bottomNavigation,
+//                getSupportFragmentManager(),
+//                navResourceIds,
+//                R.id.nav_host_container
+//        );
+//    }
 
 }
--- app/src/main/java/rs/ac/bg/etf/running/login/LoginFragment.java
+++ app/src/main/java/rs/ac/bg/etf/running/login/LoginFragment.java
@@ -5,31 +5,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import dagger.hilt.android.AndroidEntryPoint;
 import rs.ac.bg.etf.running.MainActivity;
 import rs.ac.bg.etf.running.R;
+import rs.ac.bg.etf.running.data.User;
 import rs.ac.bg.etf.running.databinding.ActivityLoginBinding;
 import rs.ac.bg.etf.running.databinding.FragmentLoginBinding;
 import rs.ac.bg.etf.running.databinding.FragmentRouteBrowseBinding;
+import rs.ac.bg.etf.running.users.UserViewModel;
 
 @AndroidEntryPoint
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
+    private UserViewModel userViewModel;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
     }
 
     @Nullable
@@ -40,11 +51,55 @@ public class LoginFragment extends Fragment {
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);}
+                String username = binding.username.getEditableText().toString();
+                String password = binding.password.getEditableText().toString();
+
+                try {
+                    User user = getUser(username);
+                    if(user != null && checkPassword(user, password)){
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getContext(), R.string.user_doesnt_exist, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
         });
 
         return binding.getRoot();
+    }
+
+    private User getUser(String username) throws ExecutionException, InterruptedException {
+        Callable<User> callable = new Callable<User>() {
+            @Override
+            public User call() throws Exception {
+                return userViewModel.findUserByUsername(username);
+            }
+        };
+
+        Future<User> future = Executors.newSingleThreadExecutor().submit(callable);
+
+        return future.get();
+    }
+
+    private Boolean checkPassword(User user, String password) throws ExecutionException, InterruptedException {
+        Callable<Boolean> callable = new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return user.getPassword().equals(password);
+            }
+        };
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(callable);
+
+        return future.get();
     }
 
 }
